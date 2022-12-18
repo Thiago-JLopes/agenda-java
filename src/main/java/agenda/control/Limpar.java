a53dd29edadf4f40967d986bc2092403ab8fe0ba
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2022-12-17 22:58:34 -0300

    Cria método para limpar o formulário.

---
+++ src/main/java/agenda/control/Limpar.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package agenda.control;
+
+import agenda.model.Contato;
+import agenda.view.Tela;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author thiago
+ */
+public class Limpar implements ActionListener{
+    private Tela tela;
+
+    public Limpar(Tela tela) {
+        this.tela = tela;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        tela.getJtNome().setText("");
+        tela.getJtTelefone().setText("");
+        tela.getJtDescricao().setText("");
+        tela.getJtEmail().setText("");
+        
+        tela.repaint();
+    }
+    
+}
